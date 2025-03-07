aed5fe2a995cb1ce8d6901b544de98bbe459a42c
tarun <imtarungupta23@gmail.com>
2025-03-07 18:44:28 +0530

    refactor logout options to set httpOnly to true and add sameSite attribute for cross-origin handling

--- server/src/controller/user.controller.js
+++ server/src/controller/user.controller.js
@@ -142,9 +142,10 @@ const logout = asyncHandler(async (req, res) => {
     )
 
     const options = {
-        httpOnly: false,
+        httpOnly: true,
         secure: true,  // Use true if deployed with HTTPS, false for local development
         // Must be "None" for cross-origin cookies
+        sameSite: "None"
     };
 
 
